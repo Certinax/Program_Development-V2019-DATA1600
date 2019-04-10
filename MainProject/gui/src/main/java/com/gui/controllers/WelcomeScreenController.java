a2bcec6e144c5fe906a9bf30a4e786f77a018496
Certinax <s319217@oslomet.no>
2019-04-10 14:00:16 +0200

    testing

--- MainProject/gui/src/main/java/com/gui/controllers/WelcomeScreenController.java
+++ MainProject/gui/src/main/java/com/gui/controllers/WelcomeScreenController.java
@@ -1,4 +1,37 @@
 package com.gui.controllers;
 
-public class WelcomeScreenController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WelcomeScreenController implements Initializable {
+
+    @FXML
+    Label label;
+
+    @FXML
+    Label userCreation;
+
+    @FXML
+    void validate() {
+
+    }
+
+    @FXML
+    public void confirm() {
+        if(!userCreation.isVisible()) {
+            userCreation.setVisible(true);
+        }
+        userCreation.setTextFill(Color.GREEN);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
 }
