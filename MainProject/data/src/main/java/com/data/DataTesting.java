e5b70158b4e789833355bc9ece0dbc61157425b1
FredrikPedersen <fredrikhp@gmail.com>
2019-04-15 16:01:13 +0200

    Fixed dependencies

--- MainProject/data/src/main/java/com/data/DataTesting.java
+++ MainProject/data/src/main/java/com/data/DataTesting.java
@@ -1,8 +1,17 @@
 package com.data;
 
+import com.data.clients.infoclasses.Address;
+import com.logic.exceptions.InvalidAddressException;
+
 public class DataTesting {
 
     public static void main(String[] args) {
+
+        try {
+            Address test = new Address("Haugveien 19", 1920);
+        }catch (InvalidAddressException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- MainProject/data/src/main/java/module-info.java
+++ MainProject/data/src/main/java/module-info.java
@@ -1,2 +1,4 @@
 module data {
+
+    requires logic;
 }
