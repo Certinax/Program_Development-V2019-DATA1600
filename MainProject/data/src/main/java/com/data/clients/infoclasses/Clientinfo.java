7f55b1c62f0d7df7e784046a9b11ca36221ac05b
Certinax <s319217@oslomet.no>
2019-04-16 12:14:41 +0200

    Added IndustryEnum, Clientinfo and Emoployer
    @Industry - Used for listing of possible industries in this project.

---
+++ MainProject/data/src/main/java/com/data/clients/infoclasses/Clientinfo.java
@@ -0,0 +1,18 @@
+package com.data.clients.infoclasses;
+
+/**
+ * <h1>Clientinfo</h1>
+ *
+ * This dataclass is used for abstract class client
+ *
+ * @Author Mathias Lund Ahrn
+ * @Since 16-04-19
+ */
+public class Clientinfo {
+
+    private Email email;
+    private Phonenumber phonenumber;
+
+    
+
+}
