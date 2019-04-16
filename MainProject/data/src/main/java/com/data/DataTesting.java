7a353d493608df883d283d8adee7a87f228672fb
Certinax <s319217@oslomet.no>
2019-04-16 13:08:49 +0200

    Lagt til Threadsafe ID felt i clientinfo

--- MainProject/data/src/main/java/com/data/DataTesting.java
+++ MainProject/data/src/main/java/com/data/DataTesting.java
@@ -1,8 +1,9 @@
 package com.data;
 
-import com.data.clients.infoclasses.Address;
-import com.data.clients.infoclasses.Industry;
+import com.data.clients.Client;
+import com.data.clients.infoclasses.*;
 import com.logic.exceptions.InvalidAddressException;
+import com.logic.exceptions.InvalidMailException;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
@@ -17,14 +18,38 @@ public class DataTesting {
             e.printStackTrace();
         }
 
-        ArrayList<Industry> industries = new ArrayList<>(EnumSet.allOf(Industry.class));
+        /*ArrayList<Industry> industries = new ArrayList<>(EnumSet.allOf(Industry.class));
 
         System.out.println(industries.toString());
 
         for(Industry industry: Industry.values()) {
             //System.out.println(industry);
             System.out.println(industry.getIndustryName());
+        }*/
+        Email email = null;
+        try {
+            email = new Email("test@gmail.com");
+        } catch (InvalidMailException ime) {
+            ime.printStackTrace();
         }
+
+        Phonenumber tlf = new Phonenumber(90899281);
+        Clientinfo client1 = new Clientinfo(email, tlf);
+
+        System.out.println(client1);
+
+
+        Email email2 = null;
+        try {
+            email2 = new Email("admin@hotmail.com");
+        } catch (InvalidMailException ime) {
+            ime.printStackTrace();
+        }
+
+        Phonenumber tlf2 = new Phonenumber(99231543);
+        Clientinfo client2 = new Clientinfo(email2, tlf2);
+
+        System.out.println(client2);
 
     }
 }
--- MainProject/data/src/main/java/com/data/clients/infoclasses/Clientinfo.java
+++ MainProject/data/src/main/java/com/data/clients/infoclasses/Clientinfo.java
@@ -1,9 +1,14 @@
 package com.data.clients.infoclasses;
 
+import java.util.UUID;
+
 /**
  * <h1>Clientinfo</h1>
  *
- * This dataclass is used for abstract class client
+ * This dataclass is used for abstract class Client
+ *
+ * Using Java's UUID class to generate a client identification number. This method
+ * of creating ID is Threadsafe.
  *
  * @Author Mathias Lund Ahrn
  * @Since 16-04-19
@@ -12,7 +17,39 @@ public class Clientinfo {
 
     private Email email;
     private Phonenumber phonenumber;
+    private final static UUID client_ID = UUID.randomUUID();
 
-    
+    public Clientinfo(Email email, Phonenumber phonenumber) {
+        this.email = email;
+        this.phonenumber = phonenumber;
+    }
 
+    @Override
+    public String toString() {
+        return "Clientinfo{" +
+                "email=" + email +
+                ", phonenumber=" + phonenumber +
+                ", clientID=" + client_ID +
+                '}';
+    }
+
+    public UUID getClient_ID(){
+        return client_ID;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public void setEmail(Email email) {
+        this.email = email;
+    }
+
+    public Phonenumber getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(Phonenumber phonenumber) {
+        this.phonenumber = phonenumber;
+    }
 }
