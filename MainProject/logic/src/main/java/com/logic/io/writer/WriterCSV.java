3eefba36a7a5c4380b59f3e5772624254aab17d8
Certinax <s319217@oslomet.no>
2019-04-19 19:11:53 +0200

    Writerclass can successfully read complex objects and write to csv

--- MainProject/logic/src/main/java/com/logic/io/writer/WriterCSV.java
+++ MainProject/logic/src/main/java/com/logic/io/writer/WriterCSV.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class WriterCSV {
 
@@ -78,75 +79,71 @@ public class WriterCSV {
         ArrayList<String> structure = new ArrayList<String>();
 
 
-        String[] sa;
+        String[] sa = null;
 
         try {
             Class<?> clazz = obj.getClass();
 
-            sb.append(writingCSVInfo(clazz,obj));
+            //sb.append(writingCSVInfo(clazz,obj));
 
             // Følgende utkommentert kode er under utvikling
 
             sa = writingCSVInfo(clazz, obj).split("\n");
-            //structure.addAll(Arrays.asList(sa));
-            for(String s : sa) {
-                System.out.println("CSV" + s + "----");
-            }
-            /*
-            System.out.println("Printer ut fra arrayList" + structure.toString());
-            */
+            structure.addAll(Arrays.asList(sa));
+
 
             // Making sure to get all parent classes
             Class parent = clazz.getSuperclass();
             while (parent != null && parent != Object.class) {
-                sb.append(writingCSVInfo(parent, obj));
+                sa = writingCSVInfo(parent, obj).split("\n");
+                structure.addAll(Arrays.asList(sa));
                 parent = parent.getSuperclass();
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        // TODO sortertingsløkkene nedenfor skal i egne private metoder
 
-
-        return sb.toString();
-
-        /*
-        Class classType = client.getClass();
-
-        StringBuilder sb = new StringBuilder();
-
-        // Array to keep track of all methods of the client
-        Method[] methods = classType.getDeclaredMethods();
-
-        // Writing headers for the following data
-        for(Method m : methods) {
-            if(m.getParameterTypes().length == 0) {
-                if(m.getName().startsWith("get")) {
-                    sb.append(m.getName().substring(3)).append(";");
-                } else if(m.getName().startsWith("is")) {
-                    sb.append(m.getName().substring(2)).append(";");
-                }
+        // This code sort the data from an object so all the titles comes in order based on
+        // the class hierarchy (Class A, B. B extends A. A params before B params)
+        for(int i = structure.size()-1; i >= 0; i--) {
+            if(i%2 == 0) {
+                sb.append(structure.get(i));
+                sb.append(",");
+            }
+            if(i == 0) {
+                sb.deleteCharAt(sb.length()-1);
             }
         }
-
-        sb.deleteCharAt(sb.length()-1);
         sb.append("\n");
 
-        for(Method m : methods) {
-            if(m.getParameterTypes().length == 0) {
-                if(m.getName().startsWith("get") || m.getName().startsWith("is")) {
-                    System.out.println(m.invoke(client).toString());
-                    sb.append(m.invoke(client).toString());
-                    sb.append(";");
-                }
-
+        // This code sort the data from an object so all the information comes in order
+        // based on the class hierarchy
+        for(int i = structure.size()-1; i >= 0; i--) {
+            if(i%2 != 0) {
+                sb.append(structure.get(i));
+                sb.append(",");
             }
-
+            if(i == 0) {
+                sb.deleteCharAt(sb.length()-1);
+            }
         }
+        sb.append("\n");
 
-        sb.deleteCharAt(sb.length()-1);
-        return sb.toString();*/
+        return sb.toString();
+    }
 
+
+    private static String[] trimStringEnd(String[] sa) {
+        String[] saNew = new String[sa.length];
+        for(int i = 0; i < sa.length; i++) {
+            saNew[i] = sa[i].substring(sa[i].length()).replace(",","");
+        }
+        for(String s : saNew) {
+            System.out.println(s);
+        }
+        return saNew;
     }
 
 
