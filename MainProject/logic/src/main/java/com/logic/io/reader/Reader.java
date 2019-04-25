3f77eb20f0d592be191b063c475ddd1c585c79b0
FredrikPedersen <fredrikhp@gmail.com>
2019-04-25 19:57:48 +0200

    Implemented a Reader Interface and a JOBJ Reader (Full functionality not implemented yet!)

---
+++ MainProject/logic/src/main/java/com/logic/io/reader/Reader.java
@@ -0,0 +1,8 @@
+package com.logic.io.reader;
+
+import java.util.ArrayList;
+
+public interface Reader {
+
+    ArrayList<Object> readObjects(String path);
+}
---
+++ MainProject/logic/src/main/java/com/logic/io/reader/ReaderJOBJ.java
@@ -0,0 +1,28 @@
+package com.logic.io.reader;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+public class ReaderJOBJ implements Reader {
+
+    @Override
+    public ArrayList<Object> readObjects(String path) {
+        ArrayList<Object> objectsFromFile = new ArrayList<>();
+        Object serializedObject = null;
+
+        try {
+            FileInputStream fin = new FileInputStream(path);
+            ObjectInputStream oin = new ObjectInputStream(fin);
+            serializedObject = oin.readObject();
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace(); //TODO This should send a message to the GUI!
+        }
+        objectsFromFile.add(serializedObject);
+
+        throw new UnsupportedOperationException("readObjects in ReaderJOBJ is not finnished yet");
+        //return objectsFromFile;
+    }
+}
