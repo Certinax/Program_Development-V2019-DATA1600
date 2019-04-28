e0e2f927dc0121969cedbb00a6cf8c711389f48d
Lise <s305345@stud.hioa.no>
2019-04-28 14:21:42 +0200

    Update DataValidator.java
    Removed duplicates, and stadarised method names

--- MainProject/logic/src/main/java/com/logic/utilities/validators/DataValidator.java
+++ MainProject/logic/src/main/java/com/logic/utilities/validators/DataValidator.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 /**
  * <h1>String Validator</h1>
  *
- * Class with static methods to valide Strings in the application
+ * Class with static methods to validate Strings in the application
  *
  * @author Fredrik Pedersen, Lise Sundrønning
  * @since 15-04-2019
@@ -60,43 +60,46 @@ public class DataValidator {
         return matcher.find();
     }
 
-    public static boolean adressCheck(String adress){
-        Pattern regex = Pattern.compile("^[a-zæøåA-ZÆØÅ-]+ *[0-9]*$");
-        Matcher matcher = regex.matcher(adress);
-
-        return matcher.find();
-    }
-
-    public static boolean zipcodeCheck(int zipcode){
+    public static boolean zipcodeChecker(int zipcode){
         return (zipcode > 0 && zipcode < 100_000);
     }
 
-    public static boolean zipcodeCheck(String zipcode){
+    public static boolean zipcodeChecker(String zipcode){
+        if (!isInteger(zipcode)) return false;
         Pattern regex = Pattern.compile("^[0-9]{4,5}$");
         Matcher matcher = regex.matcher(zipcode);
         return matcher.find();
     }
 
-    public static boolean cityCheck(String city){
-        return true;
-    }
-
-    public static boolean industryCheck(String industry){
-        return true;
-    }
-
-    public static boolean nameCheck(String name){
+    public static boolean cityChecker(String city){
+        if (city == null) return false;
         Pattern regex = Pattern.compile("^[a-zæøåA-ZÆØÅ-]+$");
+        Matcher matcher = regex.matcher(city);
+        return matcher.find();
+    }
+
+    public static boolean industryChecker(String industry){
+        if (industry == null) return false;
+        Pattern regex = Pattern.compile("^[a-zæøåA-ZÆØÅ-]+$");
+        Matcher matcher = regex.matcher(industry);
+        return matcher.find();
+    }
+
+    public static boolean nameChecker(String name){
+        if (name == null) return false;
+        Pattern regex = Pattern.compile("^[A-ZÆØÅ][a-zæøå -]+$");
         Matcher matcher = regex.matcher(name);
 
         return matcher.find();
     }
 
-    public static boolean ageCheck(int age){
+    public static boolean ageChecker(int age){
         return (age > 0 && age < 200);
     }
 
-    public static boolean ageCheck(String age){
+    public static boolean ageChecker(String age){
+        if (age == null) return false;
+        if (!isInteger(age)) return false;
         Pattern regex = Pattern.compile("^[0-9]{1,3}");
         Matcher matcher = regex.matcher(age);
 
@@ -108,13 +111,16 @@ public class DataValidator {
     }
 
     public static boolean salaryReqirementsIsSet(String salaryRequirement){
+        if (salaryRequirement == null) return false;
+        if (!isInteger(salaryRequirement)) return false;
         Pattern regex = Pattern.compile("^[0-9]*$");
         Matcher matcher = regex.matcher(salaryRequirement);
 
         return matcher.find();
     }
 
-    public static boolean educationCheck(ArrayList<String> education){
+    public static boolean educationChecker(ArrayList<String> education){
+        if (education == null) return false;
         for (String edu : education) {
             Pattern regex = Pattern.compile("^[a-zæøåA-ZÆØÅ 0-9-]+$");
             Matcher matcher = regex.matcher(edu);
@@ -126,7 +132,10 @@ public class DataValidator {
         return true;
     }
 
-    public static boolean workExperianceCheck(ArrayList<String> workExperiance){
+
+    // TODO all following methods need to be updated once logic is in place
+    public static boolean workExperianceChecker(ArrayList<String> workExperiance){
+        if (workExperiance == null) return false;
         for (String exp : workExperiance) {
             Pattern regex = Pattern.compile("^[a-zæøåA-ZÆØÅ 0-9-]+$");
             Matcher matcher = regex.matcher(exp);
@@ -138,7 +147,8 @@ public class DataValidator {
         return true;
     }
 
-    public static boolean workReferenceCheck(ArrayList<String> workReference){
+    public static boolean workReferenceChecker(ArrayList<String> workReference){
+        if (workReference == null) return false;
         for (String ref : workReference) {
             Pattern regex = Pattern.compile("^[a-zæøåA-ZÆØÅ._%+@ 0-9-]+$");
             Matcher matcher = regex.matcher(ref);
@@ -150,7 +160,8 @@ public class DataValidator {
         return true;
     }
 
-    public static boolean jobListChack(ArrayList<String> jobList){
+    public static boolean jobListChacker(ArrayList<String> jobList){
+        if (jobList == null) return false;
         for (String job : jobList) {
 
             // TODO change regex to fit with jobList ID
