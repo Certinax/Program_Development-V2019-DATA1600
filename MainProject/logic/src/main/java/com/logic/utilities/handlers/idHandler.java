7b3924259f283f608352d8f0cac6dadccbb197f3
Certinax <s319217@oslomet.no>
2019-04-28 18:06:23 +0200

    Create idHandler.java

---
+++ MainProject/logic/src/main/java/com/logic/utilities/handlers/idHandler.java
@@ -0,0 +1,16 @@
+package com.logic.utilities.handlers;
+
+import java.util.HashMap;
+
+public class idHandler {
+
+    HashMap<String, Integer> id;
+
+    public idHandler(HashMap<String, Integer> id) {
+        this.id = id;
+    }
+
+    public HashMap<String, Integer> getId() {
+        return id;
+    }
+}
