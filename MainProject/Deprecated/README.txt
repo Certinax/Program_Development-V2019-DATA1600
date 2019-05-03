72a98f326565182d325b25d735273dcb8adde5d4
FredrikPedersen <fredrikhp@gmail.com>
2019-05-03 07:24:09 +0200

    Removed Deprecated

--- MainProject/Deprecated/README.txt
+++
@@ -1,2 +0,0 @@
-These classes are deprecated, i.e DO NOT USE THEM.
-They are not deleted(yet) because some of their code might be valuable.
--- MainProject/Deprecated/clientsByObjects/TemplateSort.java
+++
@@ -1,9 +0,0 @@
-package com.data.clients;
-
-/**
- * This interface marks classes so it's classes get-methods are sorted
- * in desired order by using the sorter class named MethodTemplateSorter
- *
- */
-public interface TemplateSort {
-}
