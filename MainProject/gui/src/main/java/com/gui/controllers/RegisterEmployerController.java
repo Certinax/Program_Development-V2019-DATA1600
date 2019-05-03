97612caafd598bd252db2997725fc60cae304361
Lise <s305345@stud.hioa.no>
2019-05-03 03:27:35 +0200

    goto employer

--- MainProject/gui/src/main/java/com/gui/controllers/RegisterEmployerController.java
+++ MainProject/gui/src/main/java/com/gui/controllers/RegisterEmployerController.java
@@ -99,6 +99,11 @@ public class RegisterEmployerController implements Controller {
     }
 
     @FXML
+    private void goToEmployers(ActionEvent event) {
+        sceneManager.changeScene(SceneName.EMPLOYERS);
+    }
+
+    @FXML
     private void goToTakenPositions(ActionEvent event) {
         sceneManager.changeScene(SceneName.TAKENPOSITIONS);
     }
