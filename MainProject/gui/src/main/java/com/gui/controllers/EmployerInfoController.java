044bd33bf5551986d971087865c5615d7eceb82f
Lise <s305345@stud.hioa.no>
2019-05-03 02:37:40 +0200

    done with employer info

--- MainProject/gui/src/main/java/com/gui/controllers/EmployerInfoController.java
+++ MainProject/gui/src/main/java/com/gui/controllers/EmployerInfoController.java
@@ -1,8 +1,10 @@
 package com.gui.controllers;
 
+import com.data.clients.Employer;
 import com.gui.alertBoxes.ErrorBox;
 import com.gui.scene.SceneManager;
 import com.gui.scene.SceneName;
+import com.logic.utilities.DataPasser;
 import com.logic.utilities.exceptions.ExtraStageException;
 import com.logic.utilities.exceptions.NoPrimaryStageException;
 import javafx.event.ActionEvent;
@@ -19,8 +21,25 @@ public class EmployerInfoController implements Controller {
     @FXML
     Label name, address, zipcode, city, phoneNumber, email, sector, industry;
 
+    @FXML
+    private void setData(Employer employer){
+      name.setText(employer.getName());
+      address.setText(employer.getAddress());
+      zipcode.setText(String.valueOf(employer.getZipcode()));
+      city.setText(employer.getCity());
+      phoneNumber.setText(String.valueOf(employer.getPhoneNumber()));
+      email.setText(employer.getEmail());
+      industry.setText(employer.getIndustry());
+      if (employer.getPrivateSector()){
+        sector.setText("private");
+      }else {
+        sector.setText("public");
+      }
+    }
+
     @Override
     public void initialize() {
+      setData((Employer) DataPasser.getData());
     }
 
     @Override
